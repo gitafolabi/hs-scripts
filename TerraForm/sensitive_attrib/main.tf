5cbe6e145bce91ecd95ff5b0c5bfd01cb2402886
Sepulveda, Helder <Helder.Sepulveda@transunion.com>
2026-04-22 13:07:17 -0400

    testing external file script

--- TerraForm/sensitive_attrib/main.tf
+++ TerraForm/sensitive_attrib/main.tf
@@ -1,10 +1,20 @@
 variable "data" {
-  type = string
+  type      = string
   sensitive = true
   ephemeral = true
 }
 
 module "test" {
   source = "./modul"
-  data = var.data
+  data   = var.data
+}
+
+locals {
+  test = {
+    foo = "bar"
+    script = module.test.script
+  }
+}
+output "test" {
+  value = jsonencode(local.test)
 }
--- TerraForm/sensitive_attrib/modul/main.tf
+++ TerraForm/sensitive_attrib/modul/main.tf
@@ -5,15 +5,28 @@ variable "data" {
 
 locals {
   foo = {
-    "data": var.data
-    "foo": "bar"
+    "data" : var.data
+    "foo" : "bar"
   }
 }
 
 resource "terraform_data" "test" {
   triggers_replace = timestamp()
   provisioner "local-exec" {
-    when = create
+    when    = create
     command = "echo ${var.data}"
   }
+}
+
+resource "terraform_data" "external_file" {
+  triggers_replace = timestamp()
+  provisioner "local-exec" {
+    when        = create
+    interpreter = ["/bin/bash", "-c"]
+    command     = file("${path.module}/hello.sh")
+  }
+}
+
+output "script" {
+  value = file("${path.module}/hello.sh")
 }
